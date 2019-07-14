9abcaa38891ab30f0833fa40402c33bc0d9d9e7d
Mikita Kryvitski <solairerove@gmail.com>
2019-07-14 19:21:19 +0300

    feature: complete ex1 of jedi lvl 8

--- jedi1/ex1.go
+++ jedi1/ex1.go
@@ -1,10 +1,42 @@
 package jedi1
 
 import (
+	"encoding/json"
 	"fmt"
 )
+
+type user struct {
+	First string `json:"first"`
+	Age   int    `json:"age"`
+}
 
 // Ex1 tbd
 func Ex1() {
 	fmt.Println("\nEx1")
+
+	u1 := user{
+		First: "James",
+		Age:   32,
+	}
+
+	u2 := user{
+		First: "Moneypenny",
+		Age:   27,
+	}
+
+	u3 := user{
+		First: "M",
+		Age:   54,
+	}
+
+	users := []user{u1, u2, u3}
+
+	fmt.Println(users)
+
+	bs, err := json.Marshal(users)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string(bs))
 }
