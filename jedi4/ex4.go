5f9bbe459f77c037df66303e77dcc986f441033a
Mikita Kryvitski <solairerove@gmail.com>
2019-07-14 19:54:07 +0300

    feature: sort ints and strings

--- jedi4/ex4.go
+++ jedi4/ex4.go
@@ -2,9 +2,34 @@ package jedi4
 
 import (
 	"fmt"
+	"sort"
 )
 
 // Ex4 tbd
 func Ex4() {
 	fmt.Println("\nEx4")
+
+	xi := []int{5, 8, 2, 43, 17, 987, 14, 12, 21, 1, 4, 2, 3, 93, 13}
+	xs := []string{
+		"random",
+		"rainbow",
+		"delights",
+		"in",
+		"torpedo",
+		"summers",
+		"under",
+		"gallantry",
+		"fragmented",
+		"moons",
+		"across",
+		"magenta",
+	}
+
+	fmt.Println(xi)
+	sort.Ints(xi)
+	fmt.Println(xi)
+
+	fmt.Println(xs)
+	sort.Strings(xs)
+	fmt.Println(xs)
 }
