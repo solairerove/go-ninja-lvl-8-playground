77d86af0c582de5e8351315871f268eac1d69175
Mikita Kryvitski <solairerove@gmail.com>
2019-07-14 19:44:02 +0300

    feature: complete ex3

--- jedi3/ex3.go
+++ jedi3/ex3.go
@@ -1,10 +1,68 @@
 package jedi3
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
 )
+
+type user struct {
+	First   string
+	Last    string
+	Age     int
+	Sayings []string
+}
 
 // Ex3 tbd
 func Ex3() {
 	fmt.Println("\nEx3")
+
+	u1 := user{
+		First: "James",
+		Last:  "Bond",
+		Age:   32,
+		Sayings: []string{
+			"Shaken, not stirred",
+			"Youth is no guarantee of innovation",
+			"In his majesty's royal service",
+		},
+	}
+
+	u2 := user{
+		First: "Miss",
+		Last:  "Moneypenny",
+		Age:   27,
+		Sayings: []string{
+			"James, it is soo good to see you",
+			"Would you like me to take care of that for you, James?",
+			"I would really prefer to be a secret agent myself.",
+		},
+	}
+
+	u3 := user{
+		First: "M",
+		Last:  "Hmmmm",
+		Age:   54,
+		Sayings: []string{
+			"Oh, James. You didn't.",
+			"Dear God, what has James done now?",
+			"Can someone please tell me where James Bond is?",
+		},
+	}
+
+	users := []user{u1, u2, u3}
+
+	for _, u := range users {
+		fmt.Println("\n", u.First, u.Last, u.Age)
+		for _, s := range u.Sayings {
+			fmt.Println(s)
+		}
+	}
+
+	fmt.Println()
+
+	err := json.NewEncoder(os.Stdout).Encode(users)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
